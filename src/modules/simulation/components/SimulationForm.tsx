2efc7f8f28f4745749f9dde6c35cd725b9e3c3fa
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-03-11 09:51:23 +0100

    Update de installationTypeSection fonctionnelle

--- src/modules/simulation/components/SimulationForm.tsx
+++ src/modules/simulation/components/SimulationForm.tsx
@@ -1,38 +1,38 @@
 import React, { useRef, useState } from 'react'
-import { useForm } from "react-hook-form";
-import { Form } from "@digico/ui";
+import { useForm } from 'react-hook-form'
+import { Form } from '@digico/ui'
 import { NodeNavigator } from '@simulation/DAGmap/NodeNavigator'
 import { simulationMap } from '@simulation/DAGmap/SimulationMap'
 
 import { useCreateSimulation } from '@simulation/hooks/mutation/useCreateSimulation'
 import { useUpdateSimulation } from '@simulation/hooks/mutation/useUpdateSimulation'
 import { CreateSimulationType } from '@simulation/types/create-simulation-type'
-import { SimulationType } from "@simulation/types/update-simulation-type";
+import { SimulationType } from '@simulation/types/update-simulation-type'
 
 export default function SimulationForm() {
-    const form = useForm<SimulationType>();
+    const form = useForm<SimulationType>()
 
     const firstNodeId = 'installation'
-    const navigator = new NodeNavigator(simulationMap);
-    const [currentNode, setCurrentNode] = useState(navigator.getNode(firstNodeId));
-    const history = useRef([firstNodeId]);
-    const currentNodeId = useRef(firstNodeId);
-    const firstNext = useRef(false);
+    const navigator = new NodeNavigator(simulationMap)
+    const [currentNode, setCurrentNode] = useState(navigator.getNode(firstNodeId))
+    const history = useRef([firstNodeId])
+    const currentNodeId = useRef(firstNodeId)
+    const firstNext = useRef(false)
 
-    const simulationId =  sessionStorage.getItem('simulationId');
+    const simulationId = sessionStorage.getItem('simulationId')
 
-    const createSimulation = useCreateSimulation();
-    const updateSimulation = useUpdateSimulation();
+    const createSimulation = useCreateSimulation()
+    const updateSimulation = useUpdateSimulation()
 
     const test = {
-        "current_step": currentNodeId,
-        "simulation_id": simulationId
+        current_step: currentNodeId,
+        simulation_id: simulationId
     }
 
     const shouldCreate = () => {
         if (!firstNext.current && !simulationId) {
             const data: CreateSimulationType = {
-                "current_step": firstNodeId
+                current_step: firstNodeId
             }
             firstNext.current = true;
 
@@ -45,12 +45,11 @@ export default function SimulationForm() {
     }
 
     const updateSimulationFn = (data: SimulationType) => {
-
         shouldCreate();
-
+        data.current_step = navigator.getNextNodeId(currentNodeId.current);
         updateSimulation.mutate(data, {
             onSuccess: () => {
-                console.log("Simulation mise à jour (form)");
+                console.log('Simulation mise à jour (form)');
             }
         });
     }
@@ -72,13 +71,19 @@ export default function SimulationForm() {
     };
 
     const handleSubmit = () => {
-        console.log("Submit called");
+        console.log('Submit called');
     };
 
     return (
         <Form useForm={form} onSubmit={handleSubmit}>
-            { React.createElement(currentNode.component, { parentData: test, handleValue: updateSimulationFn, onValid: handleNext,
-                onBack: handleBack, onSkip: handleNext, onSubmit: handleSubmit }) }
+            {React.createElement(currentNode.component, {
+                parentData: test,
+                handleValue: updateSimulationFn,
+                onValid: handleNext,
+                onBack: handleBack,
+                onSkip: handleNext,
+                onSubmit: handleSubmit
+            })}
         </Form>
     )
 }
