88f8e70dc756ba2590abdabfdc22faebb43e05cd
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-03-12 17:00:19 +0100

    Premier draft de PossessionSection.tsx .2

--- src/modules/simulation/components/section/possession/PossessionSection.tsx
+++ src/modules/simulation/components/section/possession/PossessionSection.tsx
@@ -11,25 +11,31 @@ export default function PossessionSection(propsSection: PropsSectionType) {
     const [selectedPossessions, setSelectedPossessions] = useState<string[]>([]);
 
     return (
-        <div>
+        <div className={"flex flex-col items-center gap-[3.6rem]"}>
 
-            {possessions.map((possession) => (
-                <InputCard
-                   key={possession.id}
-                   id={possession.id}
-                   label={possession.label}
-                   value={possession.value}
-                   name={questionName}
-                   onClick={() => {
-                       setSelectedPossessions((prev) => {
-                           return prev.includes(possession.id)
-                               ? prev.filter(id => id !== possession.id)
-                               : [...prev, possession.id];
-                       });
-                   }}
-                   type={"checkbox"}
-                />
-            ))}
+            <h2>Chez moi, je possède</h2>
+
+            <div className={"flex flex-wrap justify-center gap-x-[1.7rem] gap-y-[3.3rem] max-w-[110rem]"}>
+                {possessions.map((possession) => (
+                    <InputCard
+                       key={possession.id}
+                       id={possession.id}
+                       label={possession.label}
+                       value={possession.value}
+                       name={questionName}
+                       onClick={() => {
+                           setSelectedPossessions((prev) => {
+                               return prev.includes(possession.id)
+                                   ? prev.filter(id => id !== possession.id)
+                                   : [...prev, possession.id];
+                           });
+                       }}
+                       type={"checkbox"}
+                       boxStyle={"basis-1/3 w-[35.1rem] h-[13.4rem]"}
+                       logoPath={possession.imagePath}
+                    />
+                ))}
+            </div>
 
             <StepNavigation
                 showSkip={true}
