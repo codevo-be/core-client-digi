0bd570f55808087ed9f3202d4b67ab5166ec750a
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-03-14 11:51:02 +0100

    Premier draft ContactInformationSection.tsx

--- src/modules/simulation/components/section/contactInformation/ContactInformationSection.tsx
+++ src/modules/simulation/components/section/contactInformation/ContactInformationSection.tsx
@@ -1,10 +1,10 @@
+import { useEffect } from 'react'
+import { useFormContext } from 'react-hook-form'
 import { Form } from '@digico/ui'
 import countries from '@simulation/countries.json';
 
 import { PropsSectionType } from '@simulation/components/section/PropsSectionType'
 import { StepNavigation } from '@simulation/components/StepNavigation'
-import { useEffect } from 'react'
-import { useFormContext } from 'react-hook-form'
 
 export default function ContactInformationSection(propsSection: PropsSectionType) {
 
@@ -19,22 +19,27 @@ export default function ContactInformationSection(propsSection: PropsSectionType
 
             <h2>Vos informations de contact</h2>
 
-            <Form.Group className={"w-[78.2rem] h-[46.4rem] px-[4.8rem] py-[5.1rem] bg-white border-2 border-[#9DBFD3] rounded-[1.4rem] text-[#023A65]"}>
-                <Form.Row>
-                    <Form.Field name={`company_name`} id="company_name" label="Email" placeholder="..." />
-                    <Form.Field name={`vat_number`} id="vat_number" label="Téléphone" placeholder="..." />
-                </Form.Row>
-                <Form.Row>
-                    <Form.Field name={`firstname`} id="firstname" label="Code postal" placeholder="..." />
-                    <Form.Select name={"country"} label={"Pays"} options={countries}/>
-                </Form.Row>
+            <Form.Group className={" w-[78.2rem] h-[46.4rem] px-[4.8rem] py-[5.1rem] bg-white border-2 border-[#9DBFD3] rounded-[1.4rem] text-[#023A65]"}>
 
-                <p className={"text-[1.2rem] text-[#90B1C9]"}>En cliquant sur voir mon offre j’accepte d’être recontacté(e)
-                    et que les données saisies soient exploitées dans le cadre de la relation commerciale
-                    qui pourrait découler de ce contact et conformément à nos conditions générales et vie privée.</p>
-                <p className={"text-[1.2rem] text-[#90B1C9]"}>Par la suite, si vous souhaitez modifier ou supprimer
-                    vos données, ou simplement savoir quelles données sont stockées dans notre base de données,
-                    vous pouvez envoyer un email à l'adresse suivante :</p>
+                <div className={"flex flex-col gap-[6rem]"}>
+                    <Form.Row>
+                        <Form.Field name={`company_name`} id="company_name" label="Email" placeholder="..." />
+                        <Form.Field name={`vat_number`} id="vat_number" label="Téléphone" placeholder="..." />
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Field name={`firstname`} id="firstname" label="Code postal" placeholder="..." />
+                        <Form.Select name={"country"} label={"Pays"} options={countries}/>
+                    </Form.Row>
+                </div>
+
+                <div className={"flex flex-col gap-[1rem]"}>
+                    <p className={"text-[1.2rem] text-[#90B1C9]"}>En cliquant sur voir mon offre j’accepte d’être recontacté(e)
+                        et que les données saisies soient exploitées dans le cadre de la relation commerciale
+                        qui pourrait découler de ce contact et conformément à nos conditions générales et vie privée.</p>
+                    <p className={"text-[1.2rem] text-[#90B1C9]"}>Par la suite, si vous souhaitez modifier ou supprimer
+                        vos données, ou simplement savoir quelles données sont stockées dans notre base de données,
+                        vous pouvez envoyer un email à l'adresse suivante :</p>
+                </div>
 
             </Form.Group>
 
