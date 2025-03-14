fff2a03f86125ac1092da9bb82d73cc861066aee
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-03-14 13:54:47 +0100

    amélioration de HouseSizeSection.tsx

--- src/modules/simulation/components/section/houseSize/HouseSizeSection.tsx
+++ src/modules/simulation/components/section/houseSize/HouseSizeSection.tsx
@@ -1,3 +1,5 @@
+import { Form } from '@digico/ui'
+
 import { StepNavigation } from '@simulation/components/StepNavigation'
 
 type Props = {
@@ -7,10 +9,28 @@ type Props = {
 }
 
 export default function HouseSizeSection({ handleValue, onBack, onValid }: Props) {
+
+    const questionName = ""; //TODO
+
     return(
-        <div>
-            <input placeholder={"input mètre carré"}/>
-            <input placeholder={"input dimension"}/>
+        <div className={"flex flex-col items-center gap-[5.7rem]"}>
+
+            <h2>Indiquez les dimensions totales de votre maison</h2>
+
+            <div className={"flex gap-[2.9rem]"}>
+                <Form.Group className={"bg-white p-[2.9rem] h-[44.2rem] w-[46.3rem] rounded-2xl"}>
+                    <p>Je connais le nombre de m2 de ma maison</p>
+                    <Form.Field suffix={"m2"} placeholder={"..."}/>
+                </Form.Group>
+
+                <Form.Group className={"bg-white p-[2.9rem] h-[44.2rem] w-[46.3rem] rounded-2xl"}>
+                    <p>
+                        Je connais les dimensions de ma maison
+                    </p>
+                    <Form.Field suffix={"m"} placeholder={"..."}/>
+                    <Form.Field suffix={"m"} placeholder={"..."}/>
+                </Form.Group>
+            </div>
 
             <StepNavigation showSkip={true} onSkip={() => {
                 handleValue('inputMetreCarré', 'something');
