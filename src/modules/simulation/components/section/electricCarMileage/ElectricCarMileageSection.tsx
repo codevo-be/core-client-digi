fb8b9853f5817eb6e1e78789f0cdd245c0d9c3e2
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-03-14 14:01:29 +0100

    stylisation des inputs de types range v1

--- src/modules/simulation/components/section/electricCarMileage/ElectricCarMileageSection.tsx
+++ src/modules/simulation/components/section/electricCarMileage/ElectricCarMileageSection.tsx
@@ -2,24 +2,50 @@ import { useState } from 'react'
 
 import { PropsSectionType } from '@simulation/components/section/PropsSectionType'
 import { StepNavigation } from '@simulation/components/StepNavigation'
+import { Button } from '@digico/ui'
 
 export default function ElectricCarMileageSection(propsSection: PropsSectionType) {
 
     const questionName = "electricCarMileage";
-    const [rangeValue, setRangeValue] = useState("12000");
+    const [rangeValue, setRangeValue] = useState(12000);
+
+    const min = 1000;
+    const step = 1000;
+    const max = 24000;
+
+    const minusClicked = () => {
+        const newValue = rangeValue - step;
+        if (newValue < min) setRangeValue(min)
+        else setRangeValue(newValue)
+    }
+
+    const plusClicked = () => {
+        const newValue = rangeValue + step;
+        if (newValue > max) setRangeValue(max)
+        else setRangeValue(newValue)
+    }
 
     return(
-        <div>
+        <div className={"flex flex-col items-center"}>
 
-            <p>{rangeValue} kms</p>
-            <input type={"range"} min={1000} max={24000} step={1000} onChange={(event) =>{
-                setRangeValue(event.target.value);
-            }}/>
+            <h2>Nombre de km moyen par année ?</h2>
+
+            <div className={"text-[#2A8831] flex flex-col items-center"}>
+                <p><span className={"text-[6.4rem]"}>{rangeValue}</span> kms</p>
+                <div>
+                    <Button type={"button"} onClick={minusClicked}>-</Button>
+                    <input className={"w-[51.8rem] bg-[#2A8831]"} value={rangeValue} type={'range'} min={min} max={max} step={step} onChange={(event) => {
+                        // @ts-ignore
+                        setRangeValue(event.target.value);
+                    }}/>
+                    <Button type={"button"} onClick={plusClicked}>+</Button>
+                </div>
+            </div>
 
             <StepNavigation
                 showSkip={true}
                 onSkip={() => {
-                    propsSection.handleValue(questionName, rangeValue);
+                    propsSection.handleValue(questionName, String(rangeValue));
                     propsSection.onValid()
                 }}
                 showBack={true} onBack={propsSection.onBack}
--- src/modules/simulation/components/section/hourPerDay/HourPerDaySection.tsx
+++ src/modules/simulation/components/section/hourPerDay/HourPerDaySection.tsx
@@ -1,28 +1,49 @@
 import { useState } from 'react'
+import { Button } from '@digico/ui'
 
+import { PropsSectionType } from '@simulation/components/section/PropsSectionType'
 import { StepNavigation } from '@simulation/components/StepNavigation'
 
-type Props = {
-    handleValue: (label: string, response: string) => void;
-    onSkip: () => void;
-    onBack: () =>  void;
-}
+export default function HourPerDaySection(props: PropsSectionType) {
 
-export default function HourPerDaySection({ handleValue, onSkip, onBack }: Props) {
+    const [rangeValue, setRangeValue] = useState(5);
 
-    const [rangeValue, setRangeValue] = useState("5");
+    const min = 2;
+    const step = 1;
+    const max = 9;
+
+    const minusClicked = () => {
+        const newValue = rangeValue - step;
+        if (newValue < min) setRangeValue(min)
+        else setRangeValue(newValue)
+    }
+
+    const plusClicked = () => {
+        const newValue = rangeValue + step;
+        if (newValue > max) setRangeValue(max)
+        else setRangeValue(newValue)
+    }
 
     return (
-        <div>
-            <p>{rangeValue} heures</p>
-            <input type={'range'} min={2} max={10} step={1} onChange={(event) => {
-                setRangeValue(event.target.value);
-            }}/>
+        <div className={"flex flex-col items-center"}>
+            <h2>En moyenne, combien d'heures par jour êtes-vous à la maison ?</h2>
+
+            <div className={"text-[#2A8831] flex flex-col items-center"}>
+                <p><span className={"text-[6.4rem]"}>{rangeValue}</span> heures</p>
+                <div>
+                    <Button type={"button"} onClick={minusClicked}>-</Button>
+                    <input className={"w-[51.8rem] bg-[#2A8831]"} value={rangeValue} type={'range'} min={min} max={max} step={step} onChange={(event) => {
+                        // @ts-ignore
+                        setRangeValue(event.target.value);
+                    }}/>
+                    <Button type={"button"} onClick={plusClicked}>+</Button>
+                </div>
+            </div>
 
             <StepNavigation showSkip={true} onSkip={() => {
-                handleValue('perHourValue', rangeValue);
-                onSkip();
-            }} showBack={true} onBack={onBack} showSubmit={false} />
+                props.handleValue('perHourValue', String(rangeValue));
+                props.onValid();
+            }} showBack={true} onBack={props.onBack} showSubmit={false} />
         </div>
     );
 }
